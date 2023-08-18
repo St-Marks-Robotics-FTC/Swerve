5790498d8cd225abcda7c536a83d4190b36d95b7
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-08-18 02:22:51 -0500

    should go short way around

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRservoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRservoTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.normalizeDegrees;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -17,7 +19,7 @@ import com.qualcomm.robotcore.hardware.PwmControl;
 @TeleOp
 public class CRservoTest extends LinearOpMode {
 
-    public static double servoPos = 180;
+    public static double targetPos = 180;
 
     public static String servoName = "FRsteer";
     public static String encoderName = "FRencoder";
@@ -53,7 +55,21 @@ public class CRservoTest extends LinearOpMode {
         while (opModeIsActive()) {
             servoPID.setPID(kP, kI, kD);
 
-            double output = servoPID.calculate(servoEncoder.getVoltage() / 3.3 * 360, servoPos);
+            if (gamepad1.y) {
+                targetPos = 0;
+            } else if (gamepad1.x) {
+                targetPos = 90;
+            } else if (gamepad1.a) {
+                targetPos = 180;
+            } else if (gamepad1.b) {
+                targetPos = 270;
+            }
+
+            double curPos = servoEncoder.getVoltage() / 3.3 * 360;
+
+            double error = normalizeDegrees(targetPos - curPos);
+
+            double output = servoPID.calculate(0, error);
 
             if (gamepad1.right_trigger > 0.1) {
                 output = gamepad1.right_trigger;
@@ -65,7 +81,7 @@ public class CRservoTest extends LinearOpMode {
 
 
 
-            telemetry.addData("Servo Target Position: ", servoPos);
+            telemetry.addData("Servo Target Position: ", targetPos);
             telemetry.addData("Servo Current Position: ", servoEncoder.getVoltage() / 3.3 * 360);
 
             telemetry.addData("Servo Power: ", output);
