1060cd9c9064e65592757c024bd25e998e886070
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-08-23 18:19:51 -0500

    Update SlewTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlewTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlewTest.java
@@ -7,11 +7,15 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PwmControl;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 //@Disabled
@@ -31,6 +35,22 @@ public class SlewTest extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
 
+        // Retrieve and initialize the IMU.
+        // This sample expects the IMU to be in a REV Hub and named "imu".
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        // Now initialize the IMU with this mounting orientation
+        // Note: if you choose two conflicting directions, this initialization will cause a code exception.
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        // Reset Yaw
+        imu.resetYaw();
+
+
 
         SlewRateLimiter fwdSlew = new SlewRateLimiter(fwd_rate_limit);
 
@@ -45,6 +65,8 @@ public class SlewTest extends LinearOpMode {
 
             telemetry.addData("Joystick Y: ", fwd);
             telemetry.addData("Slew Y: ", fwdSlew.calculate(fwd));
+
+            telemetry.addData("IMU Yaw: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
 
             telemetry.update();
         }
